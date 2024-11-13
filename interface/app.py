59a1f8e1180699219b8ede8e1d6994aecc252426
winiboya <w>
2024-11-13 12:09:26 -0500

    moved stats to interface

--- interface/app.py
+++ interface/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import requests
 import os
+import plotly.graph_objects as go
+import json
+import plotly
+from stats import analytics
+import requests
+
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
+
+file = "test_file.csv"
+analyze = analytics(file)
 
 
 UPLOAD_FOLDER = './uploads'
@@ -14,3 +23,11 @@ if not os.path.exists(UPLOAD_FOLDER):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/results')
+def reuslts():
+    fig1, fig2 = analyze.all()
+
+    graphJSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+    chart2 = fig2.to_html(full_html=False)
+    return render_template('results.html', graphJSON=graphJSON, chart2 = chart2, filename = file)
